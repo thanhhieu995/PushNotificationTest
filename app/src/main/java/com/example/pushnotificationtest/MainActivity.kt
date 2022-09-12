57d8f8d397b071d52e22037dbb6a5bf2ee241d05
Hieu <thanhhieu995@gmail.com>
2022-09-12 14:42:22 +0700

    Research send message to fcm server

--- app/src/main/java/com/example/pushnotificationtest/MainActivity.kt
+++ app/src/main/java/com/example/pushnotificationtest/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.pushnotificationtest
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.iid.FirebaseInstanceId
@@ -15,6 +19,12 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.http.Url
+import java.io.IOException
 
 
 const val TOPIC = "/topics/myTopic"
@@ -23,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
 
-     override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -33,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         val message = findViewById<EditText>(R.id.message)
         val btnSend = findViewById<Button>(R.id.btnSend)
 
-        btnSend.setOnClickListener{
+        btnSend.setOnClickListener {
             val edtTitle = title.text.toString()
             val edtMessage = message.text.toString()
 
@@ -47,43 +57,32 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-         //onTokenRefresh()
+        //onTokenRefresh()
     }
 
-    private val coroutineExceptionHandler = CoroutineExceptionHandler{
-            _, throwable ->
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
         throwable.printStackTrace()
     }
 
-     private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO + coroutineExceptionHandler).launch{
-        try {
-            val response = RetrofitInstance.api.postNotification(notification)
-            if (response.isSuccessful) {
+    private fun sendNotification(notification: PushNotification) =
+        CoroutineScope(Dispatchers.IO + coroutineExceptionHandler).launch {
+            try {
+                val response = RetrofitInstance.api.postNotification(notification)
+                if (response.isSuccessful) {
 //                val responseJson: String? = Gson().toJson(response)
-                Log.d(TAG, "Response: ${Gson().toJson(response)}")
+                                Log.d(TAG, "Response: ${Gson().toJson(response)}")
 //                Log.d(TAG, "Response: $responseJson")
-            } else {
-                Log.e(TAG, response.errorBody().toString())
+                } else {
+                    Log.e(TAG, response.errorBody().toString())
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, e.toString())
             }
-        } catch (e : Exception) {
-            Log.e(TAG, e.toString())
         }
-    }
+}
 
-    fun onTokenRefresh() {
-        // Get updated InstanceID token.
-        val refreshedToken = FirebaseInstanceId.getInstance().token
-        Log.d(TAG, "Refreshed token: $refreshedToken")
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken)
-    }
-
-    private fun sendRegistrationToServer(refreshedToken: String?) {
-        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-        val ref : DatabaseReference = database.getReference("server/saving-data/IDs")
-        ref.push().setValue(refreshedToken)
-    }
+private fun sendRegistrationToServer(refreshedToken: String?) {
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val ref: DatabaseReference = database.getReference("server/saving-data/IDs")
+    ref.push().setValue(refreshedToken)
 }
